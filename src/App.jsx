f549f4f4d46ed9bc16664b00def78b3f9c244fb2
Melissa <meliss.cele@gmail.com>
2024-05-29 18:08:34 -0300

    Se avanzo en el proyecto de productos modificando los archivos app.css, app.jsx e index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Component } from 'react'
+import axios from 'axios';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default class App extends Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      categoria_id: "",
+      productos:[],
+      categorias:[]
+    }
+  }
 
+  buscarCategorias(){
+   //const url = "https://productos.ctpoba.edu.ar/api/categorias"
+   const url = "http://10.0.4.103:3000/api/categorias"
+   
+    axios.get(url)
+    .then((resp) => {
+      this.setState({categorias: resp.data.categorias})
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+
+    buscarProductos(categoria_id){
+      const url = "http://10.0.4.103:3000/api/productos"
+      const config={
+    params:{categoria_id}
+    }
+    axios.get(url, config)
+      .then((resp) => {
+        this.setState({productos: resp.data.productos})
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+      
+    
+  }
+
+render(){
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <div>
+    <span>APP</span>
+  <input type="button" 
+    value="Buscar Categorias"
+    onClick={() => this.buscarCategorias()} />
+
+   <input type="button" 
+    value="Buscar Productos"
+    onClick={() => this.buscarProductos(this.state.categoria_id)} />
+
+  <select 
+    value={this.state.categoria_id}
+    onChange={(e)=> this.setState ({categoria_id:e.target.value})}
+  >
+    {
+      this.state.categorias.map((categoria,index)=>
+      <option key={categoria.id} value={categoria.id}>{categoria.nombre}</option>)
+    }
+  </select>
+  <h3>{this.state.categoria_id}</h3>
+  {this.state.productos.map((productos, index)=>
+    <span key={productos.id}> {productos.nombre}</span>)}
+  </div> 
+
   )
 }
 
-export default App
+}
